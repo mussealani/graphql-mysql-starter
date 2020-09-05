e1044dbeea20ebf2a0bb12b71f7a11e149be4487
Mohamad Rashid <mohamadrashid@MBPsomtrMohamad.lan>
2020-09-06 01:01:26 +0200

    Get GraphQl up and running

--- src/http/server.js
+++ src/http/server.js
@@ -1,9 +1,22 @@
 import config from 'config';
 import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+
+const root = { hello: () => 'Hello world!' };
 const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
-})
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
 
 const server = app.listen(3000,() => console.log('Listen on port ' + config.get('app.port')));
